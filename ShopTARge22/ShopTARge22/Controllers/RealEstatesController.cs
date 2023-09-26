4ba66fe4d077a76b6a77de91ab8e65a1978ff686
Siim Koppel <siimk999@gmail.com>
2023-09-26 18:08:28 +0300

    Initialize Realestates db context

--- ShopTARge22/ShopTARge22/Controllers/RealEstatesController.cs
+++ ShopTARge22/ShopTARge22/Controllers/RealEstatesController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge22.Data;
 
 namespace ShopTARge22.Controllers
 {
-    public class RealEstatesController : Controller
+    public class RealestatesController : Controller
     {
+        private readonly ShopTARge22Context _context;
+
+        public RealestatesController(ShopTARge22Context context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
