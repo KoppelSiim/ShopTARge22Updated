7f3c66ec67c25c0c81d82c0d86454e8025aec95a
Siim Koppel <siimk999@gmail.com>
2023-09-26 18:24:54 +0300

    Add Realestates IndexViewModel

---
+++ ShopTARge22/ShopTARge22/Models/Realestates/RealestatesIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ShopTARge22.Models.Realestates
+{
+    public class RealestatesIndexViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Address { get; set; }
+        public float SizeSqrM { get; set; }
+        public int RoomCount { get; set; }
+        public int Floor { get; set; }
+        public string BuildingType { get; set; }
+        public DateTime BuiltInYear { get; set; }
+
+    }
+}
