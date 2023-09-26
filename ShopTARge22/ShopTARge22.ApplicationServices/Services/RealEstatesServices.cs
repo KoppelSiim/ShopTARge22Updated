7388a03322b972d9d7fd99fd7137a8f94e661cc7
Siim Koppel <siimk999@gmail.com>
2023-09-26 18:15:30 +0300

    Initialize RealestatesServices db context

--- ShopTARge22/ShopTARge22.ApplicationServices/Services/RealEstatesServices.cs
+++ ShopTARge22/ShopTARge22.ApplicationServices/Services/RealEstatesServices.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ShopTARge22.Core.ServiceInterface;
+using ShopTARge22.Data;
 
 namespace ShopTARge22.ApplicationServices.Services
 {
-    public class RealEstatesServices
+    public class RealestatesServices : IRealestates
+
     {
+        private readonly ShopTARge22Context _context;
+
+        public RealestatesServices(ShopTARge22Context context)
+        {
+            _context = context;
+        }
     }
 }
