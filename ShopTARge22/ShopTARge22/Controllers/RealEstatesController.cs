94d5864b8f34cb40a700aa3506ee14016ca622d3
Siim Koppel <siimk999@gmail.com>
2023-09-27 12:23:47 +0300

    Add  Realestate Create and Update to Controller

--- ShopTARge22/ShopTARge22/Controllers/RealEstatesController.cs
+++ ShopTARge22/ShopTARge22/Controllers/RealEstatesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using ShopTARge22.ApplicationServices.Services;
 using ShopTARge22.Core.Domain;
+using ShopTARge22.Core.Dto;
 using ShopTARge22.Core.ServiceInterface;
 using ShopTARge22.Data;
 using ShopTARge22.Models.Realestates;
@@ -19,6 +20,7 @@ namespace ShopTARge22.Controllers
             _context = context;
             _realestateServices = realestatesServices;
         }
+
         public IActionResult Index()
         {
             var result = _context.Realestates
@@ -59,6 +61,92 @@ namespace ShopTARge22.Controllers
             vm.UpdatedAt = realestate.UpdatedAt;
            
             return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            RealestatesCreateUpdateViewModel result = new RealestatesCreateUpdateViewModel();
+
+            return View("CreateUpdate", result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RealestatesCreateUpdateViewModel vm)
+        {
+            var dto = new RealestateDto()
+            {
+                Id = vm.Id,
+                Address = vm.Address,
+                SizeSqrM = vm.SizeSqrM,
+                RoomCount = vm.RoomCount,
+                Floor = vm.Floor,
+                BuildingType = vm.BuildingType,
+                BuiltInYear = vm.BuiltInYear,
+                CreatedAt = vm.CreatedAt,
+                UpdatedAt = vm.UpdatedAt,
+            };
+
+            var result = await _realestateServices.Create(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index), vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var realestate = await _realestateServices.DetailsAsync(id);
+
+            if (realestate == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new RealestatesCreateUpdateViewModel();
+
+            vm.Id = realestate.Id;
+            vm.Address = realestate.Address;
+            vm.SizeSqrM = realestate.SizeSqrM;
+            vm.RoomCount = realestate.RoomCount;
+            vm.Floor = realestate.Floor;
+            vm.BuildingType = realestate.BuildingType;
+            vm.BuiltInYear = realestate.BuiltInYear;
+            vm.CreatedAt = realestate.CreatedAt;
+            vm.UpdatedAt = realestate.UpdatedAt;
+
+
+            return View("CreateUpdate", vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(RealestatesCreateUpdateViewModel vm)
+        {
+            var dto = new RealestateDto()
+            {
+                Id = vm.Id,
+                Address = vm.Address,
+                SizeSqrM = vm.SizeSqrM,
+                RoomCount = vm.RoomCount,
+                Floor = vm.Floor,
+                BuildingType = vm.BuildingType,
+                BuiltInYear= vm.BuiltInYear,
+                CreatedAt = vm.CreatedAt,
+                UpdatedAt= vm.UpdatedAt
+            };
+
+            var result = await _realestateServices.Update(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index), vm);
         }
 
 
