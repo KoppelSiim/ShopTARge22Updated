c95661bce99f6a81b7897e0c53a7d47530413150
Siim Koppel <siimk999@gmail.com>
2023-10-05 10:16:23 +0300

    Add Tests

---
+++ ShopTARge22/ShopTARge22.Realestate.Test/RealestateTest.cs
@@ -0,0 +1,25 @@
+﻿using ShopTARge22.Core.Dto;
+using ShopTARge22.Core.ServiceInterface;
+
+namespace ShopTARge22.Realestate.Test
+{
+    public class RealestateTest: TestBase
+    {
+        [Fact]
+        public async Task ShouldNot_AddEmptyRealestate_WhenReturnResult()
+        {
+            RealestateDto realestate = new();
+            realestate.Address = "asd";
+            realestate.SizeSqrM = 1024;
+            realestate.RoomCount = 5;
+            realestate.Floor = 3;
+            realestate.BuildingType = "asd";
+            realestate.BuiltInYear = DateTime.Now;
+            realestate.CreatedAt = DateTime.Now;
+            realestate.UpdatedAt = DateTime.Now;
+            
+            var result = await Svc<IRealestateServices>().Create(realestate);
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ ShopTARge22/ShopTARge22.Realestate.Test/TestBase.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.InMemory;
+using ShopTARge22.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using ShopTARge22.Realestate.Test.Macros;
+using Microsoft.EntityFrameworkCore;
+using ShopTARge22.Core.ServiceInterface;
+using ShopTARge22.ApplicationServices.Services;
+using Microsoft.Extensions.Hosting;
+using ShopTARge22.Realestate.Test.Mock;
+
+namespace ShopTARge22.Realestate.Test
+{
+    public abstract class TestBase
+    {
+        protected IServiceProvider serviceProvider { get; }
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+        public void Dispose()
+        {
+
+        }
+        protected T Svc<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+        
+        protected T Macro<T>() where T : IMacros
+        {
+            return serviceProvider.GetService<T>();
+        }
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<IRealestateServices, RealestatesServices>();
+            services.AddScoped<IFileServices, FileServices>();
+            //services.AddScoped<IHostEnvironment, MockIHostEnvironment>();
+            services.AddDbContext<ShopTARge22Context>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(e => e.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+            RegisterMacros(services);
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+            var macros = macroBaseType.Assembly.GetTypes()
+                .Where(x => macroBaseType.IsAssignableFrom(x) && !x.IsInterface
+                && !x.IsAbstract);
+
+            foreach(var macro in macros)
+            {
+                services.AddSingleton(macro);
+            }
+        }
+    }
+}
