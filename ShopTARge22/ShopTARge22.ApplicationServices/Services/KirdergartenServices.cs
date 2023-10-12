0cd8c857d7b15fb9069ab65da9c3bc27990ddad5
Siim Koppel <siimk999@gmail.com>
2023-10-12 10:51:03 +0300

    Add Kindergarten Dbcontext, Service and ServiceInterface

---
+++ ShopTARge22/ShopTARge22.ApplicationServices/Services/KirdergartenServices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARge22.Core.Domain;
+using ShopTARge22.Core.ServiceInterface;
+using ShopTARge22.Data;
+
+namespace ShopTARge22.ApplicationServices.Services
+{
+    public class KirdergartenServices: IKindergartenServices
+    {
+        private readonly ShopTARge22Context _context;
+
+        public KirdergartenServices(ShopTARge22Context context)
+        {
+            _context = context;
+        }
+        public async Task<Kindergarten> DetailsAsync(Guid id)
+        {
+            var result = await _context.Kindergartens
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
+    }
+}
