e9d5470bc9294a1cef664007eded6c2fce79e709
Siim Koppel <siimk999@gmail.com>
2023-10-12 11:00:06 +0300

    Add Kindergartens controller, dependancy

---
+++ ShopTARge22/ShopTARge22/Controllers/KindegartensController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge22.Core.ServiceInterface;
+using ShopTARge22.Data;
+
+namespace ShopTARge22.Controllers
+{
+    public class KindegartensController : Controller
+    {
+        private readonly ShopTARge22Context _context;
+        private readonly IKindergartenServices _kindergartenServices;
+
+        public KindegartensController(ShopTARge22Context context, IKindergartenServices kindergartenServices)
+        {
+            _context = context;
+            _kindergartenServices = kindergartenServices;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
