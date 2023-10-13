52e3374cdd17b9da7faded80f0fb6e22cae4e0ca
Siim Koppel <siimk999@gmail.com>
2023-10-13 11:30:28 +0300

    Add migration typofix, add Details and CreateUpdate View

---
+++ ShopTARge22/ShopTARge22.Data/Migrations/20231013082852_TypoFix.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopTARge22.Data.Migrations
+{
+    public partial class TypoFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreateAt",
+                table: "Kindergartens",
+                newName: "CreatedAt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Kindergartens",
+                newName: "CreateAt");
+        }
+    }
+}
