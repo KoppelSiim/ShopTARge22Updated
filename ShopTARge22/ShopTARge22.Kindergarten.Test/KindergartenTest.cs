338f35273a5ba5a61ef5a1e97c1b5bde30f962c7
Siim Koppel <siimk999@gmail.com>
2023-11-02 09:07:05 +0200

    Add KinderGartenTest basic struct, dependancies

---
+++ ShopTARge22/ShopTARge22.Kindergarten.Test/KindergartenTest.cs
@@ -0,0 +1,13 @@
+
+
+namespace ShopTARge22.Kindergarten.Test
+{
+    public class KindergartenTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ShopTARge22/ShopTARge22.Kindergarten.Test/TestBase.cs
@@ -0,0 +1,9 @@
+﻿
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace ShopTARge22.Kindergarten.Test
+{
+    public abstract class TestBase
+    {
+    }
+}
