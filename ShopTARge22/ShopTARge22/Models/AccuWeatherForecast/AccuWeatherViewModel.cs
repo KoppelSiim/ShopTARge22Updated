7bb6642e1398ac43a80ef82b8b2fcb1bb2ccf4e9
Siim Koppel <siimk999@gmail.com>
2023-11-22 18:44:20 +0200

    Fine tuning Weather view

--- ShopTARge22/ShopTARge22/Models/AccuWeatherForecast/AccuWeatherViewModel.cs
+++ ShopTARge22/ShopTARge22/Models/AccuWeatherForecast/AccuWeatherViewModel.cs
@@ -1,13 +1,22 @@
-﻿namespace ShopTARge22.Models.AccuWeatherForecast
+﻿using System.ComponentModel;
+
+namespace ShopTARge22.Models.AccuWeatherForecast
 {
     public class AccuWeatherViewModel
     {
+        [DisplayName("City")]
         public string CityName { get; set; }
+        [DisplayName("Temperature")]
         public double? Temperature { get; set; }
+        [DisplayName("Temp Feels like")]
         public double TempFeelsLike { get; set; }
+        [DisplayName("Humidity")]
         public int? Humidity { get; set; }
+        [DisplayName("Pressure")]
         public double Pressure { get; set; }
+        [DisplayName("Wind Speed")]
         public double WindSpeed { get; set; }
+        [DisplayName("Weather Condition")]
         public string WeatherConditions { get; set; }
     }
 }
