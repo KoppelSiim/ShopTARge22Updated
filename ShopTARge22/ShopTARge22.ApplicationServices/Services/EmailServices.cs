9fb9e1ad585eddda48df98d6ebc901f3041c539a
Siim Koppel <siimk999@gmail.com>
2023-11-23 10:59:47 +0200

    Implement SendEmail method

--- ShopTARge22/ShopTARge22.ApplicationServices/Services/EmailServices.cs
+++ ShopTARge22/ShopTARge22.ApplicationServices/Services/EmailServices.cs
@@ -1,4 +1,5 @@
-﻿using MimeKit;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
 using ShopTARge22.Core.Dto;
 using ShopTARge22.Core.ServiceInterface;
 
@@ -8,10 +9,21 @@ namespace ShopTARge22.ApplicationServices.Services
     {
         public void SendEmail(EmailDtos request)
         {
-            /*var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse("fromtest"));
-            email.To.Add(MailboxAddress.Parse("totest"));
-            email.Subject*/
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(request.From));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = request.Body};
+
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Connect("smtp.gmail.com", 587, false);
+
+                // Note: only needed if the SMTP server requires authentication
+                smtp.Authenticate("smtp_username", "smtp_password");
+                smtp.Send(email);
+                smtp.Disconnect(true);
+            }
         }
     }
 }
