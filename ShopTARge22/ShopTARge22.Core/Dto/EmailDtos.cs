783b3db3beadb666cc9a4b7ee05cf8ced685a3cd
Siim Koppel <siimk999@gmail.com>
2023-11-23 10:16:48 +0200

    Email dto

---
+++ ShopTARge22/ShopTARge22.Core/Dto/EmailDtos.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopTARge22.Core.Dto
+{
+    public class EmailDtos
+    {
+        public string To { get; set; } 
+        public string Subject { get; set; } 
+        public string Body { get; set; } 
+    }
+}
