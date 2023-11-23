156f1f1c1ec2bc7f2066b0de8acc23b8529546e9
Siim Koppel <siimk999@gmail.com>
2023-11-23 10:42:43 +0200

    Email viewModel, dto, service start

--- ShopTARge22/ShopTARge22.ApplicationServices/Services/EmailServices.cs
+++ ShopTARge22/ShopTARge22.ApplicationServices/Services/EmailServices.cs
@@ -1,9 +1,17 @@
-﻿using ShopTARge22.Core.ServiceInterface;
+﻿using MimeKit;
+using ShopTARge22.Core.Dto;
+using ShopTARge22.Core.ServiceInterface;
 
 namespace ShopTARge22.ApplicationServices.Services
 {
     public class EmailServices : IEmailServices
     {
-
+        public void SendEmail(EmailDtos request)
+        {
+            /*var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("fromtest"));
+            email.To.Add(MailboxAddress.Parse("totest"));
+            email.Subject*/
+        }
     }
 }
--- ShopTARge22/ShopTARge22.Core/Dto/EmailDtos.cs
+++ ShopTARge22/ShopTARge22.Core/Dto/EmailDtos.cs
@@ -2,6 +2,7 @@
 {
     public class EmailDtos
     {
+        public string From { get; set; }
         public string To { get; set; } 
         public string Subject { get; set; } 
         public string Body { get; set; } 
