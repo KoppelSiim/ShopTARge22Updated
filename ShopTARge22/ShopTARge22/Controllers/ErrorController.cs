6191786e67df34178bc11aefd618d0013577b81b
Siim Koppel <siimk999@gmail.com>
2023-12-14 12:07:38 +0200

    Error controller partial, not found viw

--- ShopTARge22/ShopTARge22/Controllers/ErrorController.cs
+++ ShopTARge22/ShopTARge22/Controllers/ErrorController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace ShopTARge22.Controllers
 {
     public class ErrorController : Controller
     {
+
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
+
         public IActionResult HttpStatusErrorCodeHandler(int statuscode)
         {
-            return View();
+
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeActionResult>();
+
+            switch (statusCodeResult)
+            {
+
+               // case 404:
+
+            }
+
+            return View("NotFound");
         }
+
+
+        /* [Route("Error")]
+         AllowAn*/
+
+
+       /* [Route("Error")]
+        [AllowAnonymous]
+
+        public IActionResult Error()
+        {
+
+        }
+        */
     }
 }
