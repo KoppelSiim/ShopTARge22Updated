0fd1b310dafe6997f2265e5b0a7d25d460461117
Siim Koppel <siimk999@gmail.com>
2023-12-21 10:36:12 +0200

    Fix reference errors in program.cs and .csproj

--- ShopTARge22/ShopTARge22/Program.cs
+++ ShopTARge22/ShopTARge22/Program.cs
@@ -1,10 +1,11 @@
-using ShopTARge22.Hubs; // Reference the Hubs folder
 using ShopTARge22.Data;
 using Microsoft.EntityFrameworkCore;
 using ShopTARge22.Core.ServiceInterface;
 using ShopTARge22.ApplicationServices.Services;
 using Microsoft.Extensions.FileProviders;
+using ShopTARge22.Hubs;
 using Microsoft.AspNetCore.Identity;
+using ShopTARge22.Core.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,14 +22,11 @@ builder.Services.AddScoped<IEmailServices, EmailServices>();
 builder.Services.AddDbContext<ShopTARge22Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-/*
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-{
-    options.User.RequireUniqueEmail = false;
-})
-   // .AddEntityFrameworkStores<Providers.Database.EFProvider.DataContext>()
-  //  .AddDefaultTokenProviders();
-*/
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<ShopTARge22Context>()
+        .AddDefaultTokenProviders()
+        .AddDefaultUI();
 
 var app = builder.Build();
 
